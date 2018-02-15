1d1802f5d7f51648a3292c54b499a24fcf32cada
Bradley Weston <brad@legalweb.org.uk>
2018-02-15 09:42:10 +0000

    Add prefix to annotation constants.

--- annotations.go
+++ annotations.go
@@ -1,8 +1,8 @@
 package capis
 
 const (
-	// HighlightedProduct this annotation will set the highlighted
-	// flag on the product in the exported table data.
+	// AnnotationHighlightedProduct this annotation will set the
+	// highlighted flag on the product in the exported table data.
 	// Valid values are `"true"` and `true`.
-	HighlightedProduct = "comparisonapis/products/highlighted"
+	AnnotationHighlightedProduct = "comparisonapis/products/highlighted"
 )
