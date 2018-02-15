ca509d95d2078b04e81a319408b729ac636c6bbb
Bradley Weston <brad@legalweb.org.uk>
2018-02-15 09:39:07 +0000

    Add annotations.

---
+++ annotations.go
@@ -0,0 +1,8 @@
+package capis
+
+const (
+	// HighlightedProduct this annotation will set the highlighted
+	// flag on the product in the exported table data.
+	// Valid values are `"true"` and `true`.
+	HighlightedProduct = "comparisonapis/products/highlighted"
+)
